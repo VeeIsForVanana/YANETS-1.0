5fe3e02a5df37ddb09a770659409288020c744bc
VeeIsForVanana <victorereyesgg@gmail.com>
2022-01-11 16:24:34 +0800

    Removed physical_parts.py. Separate inventory for integral parts of organisms now to be implemented just with the inventory class.

--- components/physical_parts.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from inventory import Inventory
-
-class physical_parts(Inventory):
-    """
-    A class similar to an Inventory item, but not allowing consumption due to these being the actual body parts or
-    organic matter of the entity.
-    """
