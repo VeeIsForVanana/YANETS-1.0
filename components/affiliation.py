06882c9c1fec67ee663153d3c3199c2a0ae8960e
VeeIsForVanana <victorereyesgg@gmail.com>
2022-01-12 19:21:05 +0800

    Modified affiliation.py to add new affiliations for natural food chain.

--- components/affiliation.py
+++ components/affiliation.py
@@ -7,5 +7,9 @@ from enum import Enum, auto
 class Affiliations(Enum):
     player = auto()
     einsof = auto()
-    nature = auto()
+    neutral = auto()
+    plants = auto()             # Plants under the nature faction.
+    tier1_consumer = auto()     # Consumers under the nature faction. Eat plants.
+    tier2_consumer = auto()     # Consumers under the nature faction. Eat tier 1.
+    tier3_consumer = auto()     # Consumers under the nature faction. Eat tier 1 and tier 2.
     generic_hostile = auto()
